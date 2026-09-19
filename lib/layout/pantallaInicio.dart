c2813e881849c503eec2517926c66db1ca445dd8
kevinj18 <kevinjimenez839@gmail.com>
2026-09-19 14:02:43 -0500

    Se agregó en la pantalla de inicio la estructura inicial de AppBar, en el body se sgregó el ListView.builder inicial y se añadió la estructura de un bottomNavigationBar

--- lib/layout/pantallaInicio.dart
+++ lib/layout/pantallaInicio.dart
@@ -6,13 +6,44 @@ class PantallaInicio extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text('Mercado Campesino'),
+            Text(
+              'Hola, Bienvenid@',
+              style: TextStyle(fontSize: 12),
+            ),
+          ],
+        ),
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text(
-              'Bienvenido a Mercado Campesino',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                const productores = [
+                  ('correo@example.com', 'Descripcion', '2 km'),
+                  ('otro@example.com', 'Descripcion', '3.5 km'),
+                  ('tercero@example.com', 'Descripcion', '5 km'),
+                  ('cuarto@example.com', 'Descripcion', '6.2 km'),
+                  ('quinto@example.com', 'Descripcion', '8 km'),
+                ];
+                final productor = productores[index];
+
+                return ListTile(
+                  leading: const Icon(Icons.person),
+                  title: Text(productor.$1),
+                  subtitle: Text(
+                    'Vereda: ${productor.$2} · Distancia: ${productor.$3}',
+                  ),
+                );
+              },
             ),
             const SizedBox(height: 20),
             ElevatedButton(
@@ -24,6 +55,26 @@ class PantallaInicio extends StatelessWidget {
           ],
         ),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Inicio',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Carrito',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Perfil',
+          ),
+        ],
+        currentIndex: 0,
+        onTap: (index) {
+          // Acción al seleccionar un elemento del BottomNavigationBar
+        },
+      )
     );
   }
 }
