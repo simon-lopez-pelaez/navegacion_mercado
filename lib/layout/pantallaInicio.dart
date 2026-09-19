a681e40d518ba2faaf15150da422d4023027f791
kevinj18 <kevinjimenez839@gmail.com>
2026-09-19 13:39:56 -0500

    Se creó la pantalla de inicio

---
+++ lib/layout/pantallaInicio.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class PantallaInicio extends StatelessWidget {
+  const PantallaInicio({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Bienvenido a Mercado Campesino',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Acción al presionar el botón
+              },
+              child: const Text('Explorar Productos'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
